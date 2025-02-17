295aaab8b4fd47bc980a2f792dc6ad311eaa0083
RosyPetrella <petrellamr@gmail.com>
2025-02-17 16:13:25 +0100

    ex. 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,23 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e 
+restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+function countVowels(string) {
+  let count = 0;
+  const vowels = "aeiou";
+  for (let i = 0; i < string.length; i++) {
+    const thisLetter = string[i];
+    console.log(thisLetter);
+    if (vowels.includes(thisLetter)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(countVowels(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
